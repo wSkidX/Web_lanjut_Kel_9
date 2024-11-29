9692b02f8700cdfb2713cf98815ace5b9c5e16bc
Afdila-Sandi <afdilasandi23@gmail.com>
2024-11-29 18:07:58 +0700

    membuat crud kategori

---
+++ backend/prosesKategori.php
@@ -0,0 +1,64 @@
+<?php
+include 'koneksi.php';
+
+if ($_GET['proses'] == 'insert') {
+    if (isset($_POST['submit'])) {
+        try {
+            $sql = "INSERT INTO kategori (nama_kategori, keterangan) 
+                    VALUES (?, ?)";
+            $stmt = $dbh->prepare($sql);
+            $result = $stmt->execute([
+                $_POST['nama_kategori'],
+                $_POST['keterangan']
+            ]);
+
+            if ($result) {
+                echo "<script>alert('Data Berhasil Ditambahkan'); 
+                      window.location.href='../index.php?p=kategori';</script>";
+            }
+        } catch(PDOException $e) {
+            echo "<script>alert('Data Gagal Ditambahkan'); 
+                  window.location.href='../index.php?p=kategori&aksi=input';</script>";
+        }
+    }
+}
+
+if ($_GET['proses'] == 'delete') {
+    try {
+        $stmt = $dbh->prepare("DELETE FROM kategori WHERE id = ?");
+        $hapus = $stmt->execute([$_GET['id']]);
+        
+        if ($hapus) {
+            header("Location: ../index.php?p=kategori");
+        }
+    } catch(PDOException $e) {
+        echo "<script>alert('Data Gagal Dihapus'); 
+              window.location.href='../index.php?p=kategori';</script>";
+    }
+}
+
+if ($_GET['proses'] == 'edit') {
+    if (isset($_POST['submit'])) {
+        try {
+            $sql = "UPDATE kategori SET 
+                    nama_kategori = ?, 
+                    keterangan = ? 
+                    WHERE id = ?";
+            
+            $stmt = $dbh->prepare($sql);
+            $result = $stmt->execute([
+                $_POST['nama_kategori'],
+                $_POST['keterangan'],
+                $_POST['id']
+            ]);
+
+            if ($result) {
+                echo "<script>window.location.href='../index.php?p=kategori'</script>";
+            }
+        } catch(PDOException $e) {
+            echo "<script>alert('Data Gagal Diperbarui'); 
+                  window.location.href='../index.php?p=kategori&aksi=edit&id=".$_POST['id']."';</script>";
+        }
+    }
+}
+?>
